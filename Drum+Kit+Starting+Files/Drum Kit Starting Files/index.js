718f10cb0e22f01d36f584df5e67e4c1a3f321d7
Grand-Merlin <mignon.francois@gmail.com>
2024-12-11 19:08:49 +0100

    exo drum start

---
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,7 @@
+var buttonArray = document.querySelectorAll(".drum");
+for(var i=0;i < buttonArray.length;i++){
+    buttonArray[i].addEventListener("click", function (){alert("J'ai cliqué")});
+}
+
+
+
