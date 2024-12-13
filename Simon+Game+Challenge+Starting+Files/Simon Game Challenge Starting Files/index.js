e3ad30a48a50dd9c6703173cf9c20f9381f27441
Grand-Merlin <mignon.francois@gmail.com>
2024-12-13 17:45:06 +0100

    debut projet simon

---
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
@@ -0,0 +1,51 @@
+//On attache les evenements sons et animation a tous les bouttons
+for(var i = 0; i < document.querySelectorAll(".btn").length; i++){
+    document.querySelectorAll(".btn")[i].addEventListener("click", function(){
+        makeSound(this.id);
+        buttonAnimation(this);
+    });
+}
+
+//Presser la touche A pour debuter
+document.addEventListener("keypress", function(e){
+    if (e.key === "a") {
+        document.querySelector("h1").innerHTML = "Level 1";
+    }
+});
+
+// logique de jeu
+//un tableau de mes boutton
+var buttons = document.querySelectorAll(".btn");
+//tier un nombre entier de 1 a 4
+var random = Math.floor(Math.random() * 4) + 1;
+//choisi le boutton correcpondant dans le tableau poour le mettre dans la serie du jeu
+var serieJeu = [];
+serieJeu.push(buttons[random]);
+
+
+function buttonAnimation(button){
+    button.classList.toggle("pressed");
+    setTimeout(()=>{
+        button.classList.toggle("pressed");
+    },100);
+}
+
+function makeSound(colorId){
+    switch (colorId) {
+        case "red": 
+            new Audio("sounds/red.mp3").play();
+            break;
+        case "blue":
+            new Audio("sounds/blue.mp3").play();
+            break;
+        case "green":
+            new Audio("sounds/green.mp3").play();
+            break;
+        case "yellow":
+            new Audio("sounds/yellow.mp3").play();
+            break;
+        default:
+            new Audio("sounds/wrong.mp3").play();
+            break;
+    }
+};
