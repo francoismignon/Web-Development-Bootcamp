8a3cd20da4f7f3d01c9a9bcd3d0475771452505e
Grand-Merlin <mignon.francois@gmail.com>
2024-12-15 19:24:36 +0100

    continue simon project

--- Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
+++ Simon+Game+Challenge+Starting+Files/Simon Game Challenge Starting Files/index.js
@@ -1,50 +1,74 @@
 //Variables
 var buttons = document.querySelectorAll(".btn");
 var colors = [];
-var simonSequence = ["yellow", "blue", "red"];
+var simonSequence = [];
 var playerSequence = [];
 var lvl = 1;
 var message = document.querySelector("h1");
-//appuier sur une touche pour debuter
-document.addEventListener("keypress", () => {
-  //initialise le level
-  message.innerHTML = "Level " + lvl;
-  //initialiser la sequence
-  addColorToSimonSequence();
-  // le joueur dois jouer la sequence
-  checkSequencePlayer(playerSequence);
-});
+//initialisation
+pressAkeyToStart();
+
+function pressAkeyToStart() {
+  document.addEventListener("keypress", () => {
+    //initialise le level
+    message.innerHTML = "Level " + lvl;
+    //initialiser la sequence
+    addColorToSimonSequence();
+  });
+};
+
 //attacher les evenement clique sur les bouton
 for (var i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener("click", function () {
+  buttons[i].addEventListener("click", function() {
     makeSound(this.id);
     makeAnimation(this.id);
-    playerSequence.push(this.id);4
   });
   // un tableau avec uniquement la liste des 4 coloeur dispo (plus facile a manipuler)
   colors.push(buttons[i].id);
 };
 
+//le joueur va choisir sa sequence
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function (e) {
+    if (e.isTrusted) {
+      playerSequence.push(this.id);
+      if (playerSequence.length === simonSequence.length) {
+        checkSequencePlayer(playerSequence);
+      }
+    }
+  });
+};
 
 //Fonction pour verifier l'ordre de sequence du joueur
 function checkSequencePlayer(playerSequence) {
-  for (var i = 0; i < simonSequence.length; i++) {
-    if (playerSequence[i] != simonSequence[i]) {
-      console.log("Game-over");
-    }
-    else {
-      addColorToSimonSequence();
-    }
+  for (var i = 0; i < playerSequence.length; i++) {
+      if (playerSequence[i] != simonSequence[i]) {
+        gameOver();
+      }
   };
+  message.innerHTML = "Level " + ++lvl;
+  playerSequence.length = 0;
+  addColorToSimonSequence();
 };
 
+//Fonction gameOver
+function gameOver(){
+  simonSequence.length = 0;
+  playerSequence.length = 0;
+  message.innerHTML = "Game Over, Press Any Key to Restart";
+  new Audio ('sounds/wrong.mp3').play();
+  document.querySelector("html body").classList.toggle("game-over");
+  setTimeout(()=>{
+    document.querySelector("html body").classList.toggle("game-over");
+  },100)
+};
 //Fonction qui permet de jouer la sequence du simon
 function playSimonSequence() {
+
   for (var i = 0; i < simonSequence.length; i++) {
-    setTimeout(function(index){
-      console.log(document.querySelector("." + simonSequence[index]).id);
-      document.querySelector("." + simonSequence[index]).click();
-    },1000*i, i);
+    setTimeout(()=>{
+      document.querySelector("." + simonSequence[simonSequence.length - 1]).click();
+    },500);
   };
 };
 
