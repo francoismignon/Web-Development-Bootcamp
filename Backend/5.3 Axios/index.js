c82607c3e1ce9a1a43ea46c9a7b5070e0b755c22
Grand-Merlin <mignon.francois@gmail.com>
2025-01-04 18:47:34 +0100

    debut exo Authentication API

--- Backend/5.3 Axios/index.js
+++ Backend/5.3 Axios/index.js
@@ -14,19 +14,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
-    // console.log(response.data);
+    console.log(response);
     const result = response.data;
     res.render("index.ejs", { data: result });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
-      error: error.message,
+      error: error.status,
     });
   }
 });
 
 app.post("/", async (req, res) => {
-  console.log(req.body);
+  console.log(req.body.type);
+  console.log(req.body.participants);
+  var random;
+  try {
+    const response = await axios.get(`https://bored-api.appbrewery.com/filter?type=${req.body.type}&participants=${req.body.participants}`);
+    const result = response.data;
+    random = Math.floor(Math.random() * result.length)
+    // console.log(result);
+    res.render("index.ejs", {data: result[random]});
+  } catch (error) {
+    console.log("Failed to make request:", error.message)
+    res.render("index.ejs", {error: error.status});
+  }
 
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. Making
