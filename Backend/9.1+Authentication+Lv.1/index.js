944153538162177067a73e189812b672deeb840d
Francois <mignon.francois@gmail.com>
2025-03-19 12:31:47 +0100

    feat(authentication): Vérifie l'existence d'un utilisateur et met à jour les dépendances
    Ajoute un contrôle d'existence d'utilisateur dans la route /register et met à jour axios, pg, node et TypeScript. Pas de breaking change.

--- Backend/9.1+Authentication+Lv.1/index.js
+++ Backend/9.1+Authentication+Lv.1/index.js
@@ -32,12 +32,26 @@ app.get("/register", (req, res) => {
 app.post("/register", async (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
-  const response = await db.query(
-    'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *', 
-    [username, password]
-  );
-  console.log(response.rows[0]);
-  res.render("secrets.ejs");
+
+  try {
+    const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+      username
+    ]);
+  
+    if (checkResult.rows.length > 0) {
+      res.send("l'utilisateur existe déja");
+    } else {
+      const response = await db.query(
+        'INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *',
+        [username, password]
+      );
+      console.log(response.rows[0]);
+      res.render("secrets.ejs");
+    }
+  } catch (error) {
+    console.error(error);
+    throw new Error("ceci est l'erreur");
+  }
 });
 
 app.post("/login", async (req, res) => {
