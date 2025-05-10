b1999aa13ef9069a024847717846b01ccc7166bd
Mignon François <mignon.francois@gmail.com>
2025-05-10 16:39:33 +0200

    implementation route login

--- Backend/9.2+Authentication+Lv.2/index.js
+++ Backend/9.2+Authentication+Lv.2/index.js
@@ -64,7 +64,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
   const email = req.body.username;
-  const password = req.body.password;
+  const loginPassword = req.body.password;
 
   try {
     const result = await db.query("SELECT * FROM users WHERE email = $1", [
@@ -72,13 +72,19 @@ app.post("/login", async (req, res) => {
     ]);
     if (result.rows.length > 0) {
       const user = result.rows[0];
-      const storedPassword = user.password;
+      const storedHasedPassword = user.password;
 
-      if (password === storedPassword) {
-        res.render("secrets.ejs");
-      } else {
-        res.send("Incorrect Password");
-      }
+      bcrypt.compare(loginPassword, storedHasedPassword,(err, result)=>{
+        if (err) {
+          console.log("Erreur dans la comparaison des password", err);
+        } else {
+          if(result){
+            res.render("secrets.ejs");
+          }else{
+            res.send("Incorrect Password");
+          }
+        }
+      });
     } else {
       res.send("User not found");
     }
@@ -88,5 +94,5 @@ app.post("/login", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running on port : http://localhost:${port}`);
 });
