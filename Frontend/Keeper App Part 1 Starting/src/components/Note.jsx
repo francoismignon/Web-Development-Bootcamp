be6d5bc5167129f9feb47c9d81e266b34f0c8e1e
Mignon François <mignon.francois@gmail.com>
2025-05-14 18:44:09 +0200

    React exo keeper

---
+++ Frontend/Keeper App Part 1 Starting/src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note (){
+    return (
+        <div className="note">
+            <h1>Titre de la note</h1>
+            <p>Contenu de la note</p>
+        </div>
+    );
+}
+export default Note;
