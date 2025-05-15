a6f2f9341b02d6ff5e78b30aaef1ef72caba4c89
Mignon François <mignon.francois@gmail.com>
2025-05-15 21:52:29 +0200

    outils dev react + creation de nouveau components

---
+++ Frontend/react-devtools-forked/src/components/Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Info(props) {
+    return (
+        <div>
+            <p className="info">{props.infoDetails}</p>
+        </div>
+    );
+}
+export default Info;
