f588c1d90f006bf37039f8ef6255679ef4f7d19b
Mignon François <mignon.francois@gmail.com>
2025-05-17 12:46:30 +0200

    exo keeper part 2

---
+++ Frontend/keeper-app-part-2-starting-forked/src/notes.js
@@ -0,0 +1,39 @@
+const notes = [
+  {
+    key: 1,
+    title: "Délégation",
+    content:
+      "Q. Combien de programmeurs faut-il pour changer une ampoule ? R. Aucun – C'est un problème matériel."
+  },
+  {
+    key: 2,
+    title: "Boucles",
+    content:
+      "Comment garder un programmeur sous la douche pour toujours ? Montre-lui les instructions du shampooing : Faire mousser. Rincer. Répéter."
+  },
+  {
+    key: 3,
+    title: "Tableaux",
+    content:
+      "Q. Pourquoi le programmeur a-t-il quitté son travail ? R. Parce qu’il ne comprenait pas les tableaux ('arrays')."
+  },
+  {
+    key: 4,
+    title: "Matériel vs Logiciel",
+    content:
+      "Quelle est la différence entre le matériel et le logiciel ? Tu peux frapper ton matériel avec un marteau, mais tu ne peux que crier sur ton logiciel."
+  },
+  {
+    key: 5,
+    title: "Café",
+    content:
+      "Un bon programmeur se nourrit de café et d’erreurs de compilation."
+  },
+  {
+    key: 6,
+    title: "Code propre",
+    content:
+      "Un code propre, c’est comme une blague : s’il faut l’expliquer, elle est ratée."
+  }
+];
+export default notes;
